73fa51f9473d49113d6217fe9dffa3889e1c9539
sandude-ms <robsmi_ms@hotmail.com>
2021-04-09 18:40:31 -0400

    Removed unused objects

--- scripts/Create-AzAADDSJoinedFileshare.ps1
+++ scripts/Create-AzAADDSJoinedFileshare.ps1
@@ -77,7 +77,7 @@ $Scriptblock = {
     $credential = New-Object System.Management.Automation.PSCredential -ArgumentList "Azure\$($storageAccount.StorageAccountName)", $SecureKey
 
     #Mount share to set NTFS ACLs
-    $StorageFQDN = "$($StorageAccount.StorageAccountName).file.core.windows.net"
+    $StorageFQDN = "$($StorageAccount.StorageAccountName).file.core.usgovcloudapi.net"
     $StorageUNC = "\\$StorageFQDN\$($StorageShare.Name)"
     New-PSDrive -Name Z -PSProvider FileSystem -Root $StorageUNC -Credential $credential
 
@@ -117,74 +117,114 @@ $Scriptblock = {
     
 Connect-AzAccount -Identity
         
-$CTempPath = "C:\Temp"
+$CTempPath = 'C:\Temp'
 New-Item -ItemType Directory -Path $CTempPath
 $ScriptLogActionsTimes = 'C:\Temp\ScriptActionLogTimes.txt'
 Get-Timezone | Out-File -FilePath $ScriptLogActionsTimes
 Get-Date | Out-File -append $ScriptLogActionsTimes
 "______________________________" | Out-File -append $ScriptLogActionsTimes
 
-$DomainConfigItemsZip = "$CTempPath\DomainConfigItems.zip"
-Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/Azure/WVDBlueprint/main/scripts/DomainConfigItems.zip' -OutFile $DomainConfigItemsZip
+$FSLogixGPSettingsZip = "$CTempPath\FSLogixGPSettings.zip"
+Invoke-WebRequest -Uri 'https://agblueprintsa.blob.core.windows.net/blueprintscripts/FSLogixGPSettings.zip' -OutFile $FSLogixGPSettingsZip
 
-If (Test-Path $DomainConfigItemsZip){
-Expand-Archive -LiteralPath $DomainConfigItemsZip -DestinationPath $CTempPath -ErrorAction SilentlyContinue
+If (Test-Path $FSLogixGPSettingsZip){
+Expand-Archive -LiteralPath $FSLogixGPSettingsZip -DestinationPath $CTempPath -ErrorAction SilentlyContinue
 }
 
+$WVDFSLogixSWShare = "$" + "SoftwareShare" + " = " + "'\\$ENV:ComputerName\FSLogix'"
+$WVDFSLogixSWShare | Out-File -FilePath $CTempPath\InstallFSLogixClient.ps1
+$FSLogixInstallText = @'
+$CTempPath = 'C:\Temp'
+$VDOTZIP = "$CTempPath\VDOT.zip"
+
+If (Test-Path $SoftwareShare\FSLogixAppsSetup.exe){
+ Start-Process $SoftwareShare\FSLogixAppsSetup.exe -Wait -ArgumentList "/quiet /install"
+}
+
+If (Test-Path "$SoftwareShare\VDOT.zip"){
+ New-Item -ItemType Directory -Path $CTempPath -ErrorAction SilentlyContinue
+ Copy-Item "$SoftwareShare\VDOT.zip" $CTempPath
+}
+
+If (Test-Path $VDOTZIP){
+ Expand-Archive -Path $VDOTZIP -DestinationPath $CTempPath
+}
+
+If (Test-Path "$CTempPath\Virtual-Desktop-Optimization-Tool-master\Win10_VirtualDesktop_Optimize.ps1"){
+ $VDOTString = 'C:\Temp\Virtual-Desktop-Optimization-Tool-master\Win10_VirtualDesktop_Optimize.ps1 -Optimizations All -WindowsVersion .\2009 -Verbose'
+ Invoke-Expression $VDOTString
+ Set-ExecutionPolicy -ExecutionPolicy Restricted -Force
+}
+'@
+Add-Content -Path $CTempPath\InstallFSLogixClient.ps1 -Value $FSLogixInstallText
+
+$FSLogixZip = 'C:\Temp\FSLogixSW.zip'
+$FSLogixSW = 'C:\Temp\FSLogixSW'
+$FSLogixFileShare = "$CTempPath\FSLogixSW\x64\Release"
+Invoke-WebRequest -Uri 'https://aka.ms/fslogix_download' -OutFile $FSLogixZip
+Expand-Archive -Path $FSLogixZip -DestinationPath $FSLogixSW
+New-SmbShare -Name "FSLogix" -Path "$CTempPath\FSLogixSW\x64\Release"
+
+$PolicyDefinitions = "$CTempPath\PolicyDefinitions"
+$Win10GPTemplateMSI = "$CTempPath\Win10GPTemplateFiles.msi"
+$Win10GPTemplatesFolder = "$CTempPath\Win10GPTFolder"
+# $CurrentVMName = $ENV:ComputerName
+$DeploymentPrefix = $ResourceGroupName.Split('-')[0]
 $Domain = Get-ADDomain
 $PDC = $Domain.PDCEmulator
 $FQDomain = $Domain.DNSRoot
-
-Copy-Item "$CTempPath\PolicyDefinitions" "\\$FQDomain\SYSVOL\$FQDomain\Policies" -Recurse -Force
-
 $WVDPolicy = New-GPO -Name "WVD Session Host Policy"
 $PolicyID ="{" +  $WVDPolicy.ID + "}"
 $WVDComputersOU = New-ADOrganizationalUnit -Name 'WVD Computers' -DisplayName 'WVD Computers' -Path $Domain.DistinguishedName -Server $PDC -PassThru
+$VMsToManage = (Get-ADComputer -Filter * -Server $PDC -SearchBase $WVDComputersOU.DistinguishedName -SearchScope Subtree).name
+$WVDSessionHost1UNC = "\\" + $VMsToManage[0] + "\C$\Windows\PolicyDefinitions"
+Copy-Item -Path $WVDSessionHost1UNC -Destination "\\$FQDomain\SYSVOL\$FQDomain\Policies" -Recurse -Force
+
+If (Test-Path $FSLogixSW){
+Copy-Item $FSLogixSW\fslogix.admx $PolicyDefinitions -Force
+Copy-Item $FSLogixSW\fslogix.adml "$PolicyDefinitions\en-US" -Force
+}
+
+# Acquire Virtual Desktop Optimization Tool software
+$VDOTURI = 'https://github.com/The-Virtual-Desktop-Team/Virtual-Desktop-Optimization-Tool/archive/refs/heads/master.zip'
+$VDOTZip = "$CTempPath\FSLogixSW\x64\Release\VDOT.zip"
+Invoke-WebRequest -Uri $VDOTURI -OutFile $VDOTZip
+Expand-Archive -Path $VDOTZip -DestinationPath "$FSLogixFileShare"
+
 New-GPLink -Target $WVDComputersOU.DistinguishedName -Name $WVDPolicy.DisplayName -LinkEnabled Yes
 
 $PolicyStartupFolder = "\\$FQDomain\SYSVOL\$FQDomain\Policies\$PolicyID\Machine\Scripts\Startup"
 New-Item -ItemType Directory -Path $PolicyStartupFolder
 Copy-Item "$CTempPath\InstallFSLogixClient.ps1" -Destination $PolicyStartupFolder -Force -ErrorAction SilentlyContinue
 
-$CurrentVMName = hostname
-$DeploymentPrefix = $CurrentVMName.Split('-')[0]
-$CurrentResourceGroupName = ($DeploymentPrefix +, '-sharedsvcs-rg')
+# $CurrentResourceGroupName = ($DeploymentPrefix +, '-sharedsvcs-rg')
 $DeploymentPrefixSS = ($DeploymentPrefix +,'sharedsvcs*')
-$CurrentStorageAccountName = Get-AzStorageAccount -ResourceGroup $CurrentResourceGroupName | Where-Object {($_.StorageAccountName -Like "$DeploymentPrefix*" -and $_.StorageAccountName -notlike "$DeploymentPrefixSS")}
+$CurrentStorageAccountName = Get-AzStorageAccount -ResourceGroup $ResourceGroupName | Where-Object {($_.StorageAccountName -Like "$DeploymentPrefix*" -and $_.StorageAccountName -notlike "$DeploymentPrefixSS")}
 $StorageFQDN = "$($CurrentStorageAccountName.StorageAccountName).file.core.windows.net"
 $StorageShareName = Get-AzRmStorageShare -StorageAccount $CurrentStorageAccountName
 $StorageUNC = "\\$StorageFQDN\$($StorageShareName.Name)"
 
+# Import FSLogix GP settings from an export, and set TimeZoneRedirection GP setting
 $Pattern = "\{[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}\}"
 Get-ChildItem -Path $CTempPath | Where-Object {$_.Name -match $Pattern}
 $GPOBackupGuid = (Get-ChildItem -Path $CTempPath | Where-Object { $_.Name -match $Pattern }).Name -replace "{" -replace "}"
 Import-GPO -BackupId $GPOBackupGuid -Path $CTempPath -TargetName $WVDPolicy.DisplayName
-
 Set-GPRegistryValue -Name "WVD Session Host Policy" -Key "HKLM\Software\FSLogix\Profiles" -Type STRING -ValueName "VHDLocations" -Value $StorageUNC
 
-# RDP Lockdowns/Redirection group policy settings
-# If you want to enable a setting, un-comment the respective line. More information on this topic in Readme.md
-# Set-GPRegistryValue -Name "WVD Session Host Policy" -Key "HKLM\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services" -Type DWORD -ValueName "fDisableAudioCapture" -Value 1
-# Set-GPRegistryValue -Name "WVD Session Host Policy" -Key "HKLM\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services" -Type DWORD -ValueName "fDisableCameraRedir" -Value 1
-# Set-GPRegistryValue -Name "WVD Session Host Policy" -Key "HKLM\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services" -Type DWORD -ValueName "fDisableCcm" -Value 1
-# Set-GPRegistryValue -Name "WVD Session Host Policy" -Key "HKLM\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services" -Type DWORD -ValueName "fDisableCdm" -Value 1
-# Set-GPRegistryValue -Name "WVD Session Host Policy" -Key "HKLM\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services" -Type DWORD -ValueName "fDisableClip" -Value 1
-# Set-GPRegistryValue -Name "WVD Session Host Policy" -Key "HKLM\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services" -Type DWORD -ValueName "fDisableLPT" -Value 1
-# Set-GPRegistryValue -Name "WVD Session Host Policy" -Key "HKLM\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services" -Type DWORD -ValueName "fDisablePNPRedir" -Value 1
-
+# RDP Redirection settings
 Set-GPRegistryValue -Name "WVD Session Host Policy" -Key "HKLM\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services" -Type DWORD -ValueName "fEnableTimeZoneRedirection" -Value 1
 
+# Get credentials and use those to move WVD session hosts to their own OU
 $KeyVault = Get-AzKeyVault -VaultName "*-sharedsvcs-kv"
 $DAUserUPN = (Get-AzADGroup -DisplayName "AAD DC Administrators" | Get-AzADGroupMember).UserPrincipalName
 $DAUserName = $DAUserUPN.Split('@')[0]
 $DAPass = (Get-AzKeyVaultSecret -VaultName $keyvault.VaultName -name $DAUserName).SecretValue
 $DACredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $DAUserUPN, $DAPass
 $WVDComputersOU = Get-ADOrganizationalUnit -Filter 'Name -like "WVD*"' -Server $PDC
-
 $WVDComputersToMove = Get-ADComputer -Filter * -Server $PDC| Where-Object {($_.DNSHostName -like "$DeploymentPrefix*" -and $_.DNSHostName -notlike "*mgmtvm*")}
 Foreach ($W in $WVDComputersToMove) {Move-ADObject -Credential $DACredential -Identity $W.DistinguishedName -TargetPath $WVDComputersOU.DistinguishedName -Server $PDC}
 
-$VMsToManage = (Get-ADComputer -Filter * -Server $PDC -SearchBase $WVDComputersOU.DistinguishedName -SearchScope Subtree).name
+#Force a GPUpdate now, then reboot so they can take effect, and so the Startup script can run to install FSLogix
 Foreach ($V in $VMsToManage) {Invoke-Command -Computer $V -ScriptBlock {gpupdate /force}}
 Foreach ($V in $VMsToManage) {Invoke-Command -Computer $V -ScriptBlock {shutdown /r /f /t 05}}
 
